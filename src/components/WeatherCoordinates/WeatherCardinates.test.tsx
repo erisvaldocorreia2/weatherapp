af68142176247509bf5802c9015c1b656b2e84e8
erisvaldocorreia2 <erisvaldo.correia@rethink.dev>
2021-11-21 14:21:51 -0300

    criando formulario para dados manuais

--- src/components/WeatherCoordinates/WeatherCardinates.test.tsx
+++ src/components/WeatherCoordinates/WeatherCardinates.test.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
+import React from "react";
+import { fireEvent, render, waitFor } from "@testing-library/react-native";
+import { useNavigation } from "@react-navigation/native";
 
-import WeatherCoordinates from './WeatherCoordinates';
+import WeatherCoordinates from "./WeatherCoordinates";
 
-describe('Testing WeatherCurrent component', () => {
-    it('Should render correclty', () => {
-        const wrapper = render(<WeatherCoordinates />);
-        wrapper.getByTestId('weather-cardinates');
+jest.mock("@react-navigation/native", () => {
+  return {
+    ...jest.requireActual("@react-navigation/native"),
+    useNavigation: jest.fn().mockReturnValue({ navigate: jest.fn() }),
+  };
+});
+
+describe("Testing WeatherCurrent component", () => {
+  it("Should render correclty", () => {
+    const wrapper = render(<WeatherCoordinates />);
+    wrapper.getByTestId("weather-cardinates");
+  });
+
+  it("should navigate to weather screen whith given coordinates when valid form is submited", () => {
+    const mockNavigate = jest.fn();
+    (useNavigation as jest.Mock).mockReturnValueOnce({
+      navigate: mockNavigate,
     });
+    const wrapper = render(<WeatherCoordinates />);
+
+    const fields = {
+      latitue: wrapper.getByTestId("weather-coordinates-latitude"),
+      longitude: wrapper.getByTestId("weather-coordinates-longitude"),
+    };
+
+    fireEvent.changeText(fields.latitue, "0");
+    fireEvent.changeText(fields.longitude, "0");
+
+    const button = wrapper.getByTestId("button");
+    fireEvent.press(button);
+
+    waitFor(() => {
+      expect(mockNavigate).toBeCalledWith("Weather", {
+        latitude: 0,
+        longitude: 0,
+      });
+    });
+  });
 });
--- src/components/WeatherCoordinates/WeatherCoordinates.tsx
+++ src/components/WeatherCoordinates/WeatherCoordinates.tsx
@@ -1,8 +1,159 @@
-import React from 'react';
-import { View } from 'react-native';
+/* import React from "react";
+import { View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+import { useNavigation } from "@react-navigation/native";
+import { Controller, useForm } from "react-hook-form";
+import { Button } from "..";
+import { Colors } from "../../utils/constants";
+
+type FormValues = {
+  latitude: string;
+  longitude: string;
+};
 
 const WeatherCardinates = () => {
-    return <View testID={'weather-cardinates'} />;
+  const navigation = useNavigation();
+
+  const defaultValues: FormValues = {
+    latitude: "",
+    longitude: "",
+  };
+
+  const form = useForm<FormValues>({
+    defaultValues,
+    mode: "onChange",
+  });
+
+  const handleSubmit = form.handleSubmit((values) => {
+    navigation.navigate("Weather", values);
+  });
+
+  return (
+    <View testID={"weather-cardinates"}>
+      <View
+        style={{
+          marginBottom: 15,
+        }}
+      >
+        <Controller
+          control={form.control}
+          render={({ onChange, ...props }) => (
+            <TextInput
+              {...props}
+              testID="weather-coordinates-latitude"
+              onChangeText={onChange}
+              placeholder="Latitude"
+              placeholderTextColor={Colors.GREY}
+              style={{
+                backgroundColor: "transparent",
+                margin: 5,
+                borderRadius: 10,
+                borderWidth: 1,
+                borderColor: Colors.GREY,
+                paddingHorizontal: 15,
+                paddingVertical: 8,
+                color: Colors.WHITE,
+              }}
+            />
+          )}
+          name="latitude"
+        />
+        <Controller
+          control={form.control}
+          render={({ onChange, ...props }) => (
+            <TextInput
+              {...props}
+              testID="weather-coordinates-longitude"
+              onChangeText={onChange}
+              placeholder="Longitude"
+              placeholderTextColor={Colors.GREY}
+              style={{
+                backgroundColor: "transparent",
+                margin: 5,
+                borderRadius: 10,
+                borderWidth: 1,
+                borderColor: Colors.GREY,
+                paddingHorizontal: 15,
+                paddingVertical: 8,
+                color: Colors.WHITE,
+              }}
+            />
+          )}
+          name="longitude"
+        />
+      </View>
+      <Button onPress={handleSubmit} label="Find" />
+    </View>
+  );
+};
+
+export default WeatherCardinates; */
+
+import React from "react";
+import { View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+
+import { Colors } from "../../utils/constants";
+import { Button } from "..";
+
+const WeatherCardinates = () => {
+  const navigation = useNavigation();
+  const [latitude, setLatitude] = React.useState(0);
+  const [longitude, setLongitude] = React.useState(0);
+
+  const handleSubmit = () => {
+    navigation.navigate("Weather", {
+      latitude: latitude,
+      longitude: longitude,
+    });
+  };
+
+  return (
+    <View testID={"weather-cardinates"}>
+      <View
+        style={{
+          marginBottom: 15,
+        }}
+      >
+        <TextInput
+          testID="weather-coordinates-latitude"
+          onChangeText={(e) => setLatitude(parseInt(e))}
+          placeholder="Latitude"
+          placeholderTextColor={Colors.GREY}
+          style={{
+            backgroundColor: "transparent",
+            margin: 5,
+            borderRadius: 10,
+            borderWidth: 1,
+            borderColor: Colors.GREY,
+            paddingHorizontal: 15,
+            paddingVertical: 8,
+            color: Colors.WHITE,
+          }}
+        />
+
+        <TextInput
+          testID="weather-coordinates-longitude"
+          onChangeText={(e) => setLongitude(parseInt(e))}
+          placeholder="Longitude"
+          placeholderTextColor={Colors.GREY}
+          style={{
+            backgroundColor: "transparent",
+            margin: 5,
+            borderRadius: 10,
+            borderWidth: 1,
+            borderColor: Colors.GREY,
+            paddingHorizontal: 15,
+            paddingVertical: 8,
+            color: Colors.WHITE,
+          }}
+        />
+      </View>
+      <Button onPress={handleSubmit} label="Find" />
+    </View>
+  );
 };
 
 export default WeatherCardinates;
